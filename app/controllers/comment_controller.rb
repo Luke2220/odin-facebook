55da0f2832e0253ab648a104fb8a8912630e309e
Luke <kingluke2222@live.com>
2021-04-21 16:42:07 -0700

    add commenting

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,9 +1,23 @@
 class CommentController < ApplicationController
 
     def new
-        @comment = Comment.create
-        comment_id = @comment.id
-        redirect_to user_path(current_user.id, comment_id)
+        comment_form = true
+        redirect_to user_path(current_user.id, comment_form)
     end
+
+    def create
+        @comment = Comment.new
+        @comment.content = params[:content]
+        if @comment.save!
+            redirect_to user_path(current_user, @comment.id), notice: 'Comment posted'
+        else 
+            render user_path(current_user), alert: 'Comment could not be posted'
+        end
+
+    end
+
+    private
+
+  
 
 end
